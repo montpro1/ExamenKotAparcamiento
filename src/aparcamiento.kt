f9bafb638eedb6e25ed96f04a9da5e68492248cb
DANIELCASTELAO\vmontestovar <vmontestovar@danielcastelao.org>
2019-12-04 14:12:30 +0100

    corregido

--- src/aparcamiento.kt
+++ src/aparcamiento.kt
@@ -1,5 +1,6 @@
 //clase para identificar el nombre del aparcamiento, las plazas totales que tiene y las libres para que los coches accedan
 data class aparcamiento (var nombre:String="vigo",var plazastotales:Int=200,var plazaslibres:Int=100){
 }
+//nombre es el nombre que tiene el aparcamiento
 //plazas totales es el numero de coches que entran
 //plazas libres es el numero de plazas libres en un momento determinado
